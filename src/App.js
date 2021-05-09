731f87458d412b7fbf149409c0b3f09b2d555df0
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-05-10 00:44:09 +0300

    react-router-dom, registration page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './default.scss'
 import React from 'react'
 import Header from './components/Header/Header'
 import HomePage from './components/pages/HomePage/HomePage';
+import Registration from './components/pages/Registration/Registration';
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
         <Header />
       <div className="main">
-        <HomePage />
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/registration" component={Registration}/>
       </div>
     </div>
   );

826b2aec5036a23bb5a30513800a3df5238781ca
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-05-10 12:35:18 +0300

    created firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCdj7ddC73-D_q5OG-W1pasHVyc8IGMJFA",
+    authDomain: "e-commerce-c8ec0.firebaseapp.com",
+    projectId: "e-commerce-c8ec0",
+    storageBucket: "e-commerce-c8ec0.appspot.com",
+    messagingSenderId: "87609863334",
+    appId: "1:87609863334:web:5473e5ed66fc908f731867",
+    measurementId: "G-TDDLV0CQBB"
+  };
